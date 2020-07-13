3d15eea3ef92181b1ffb98293347f41d58e5dfdd
Garry Mark <94garrymark@gmail.com>
2020-07-13 09:00:40 +0800

    chore: 使用@zeit/next-sass 支持自动引入变量与 mixins

---
+++ next.config.js
@@ -0,0 +1,20 @@
+const withSass = require('@zeit/next-sass');
+module.exports = withSass({
+    cssModules: true,
+    cssLoaderOptions: {
+        importLoaders: 1,
+        localIdentName: "[local]__[hash:base64:5]",
+    },
+    webpack: (config) => {
+        config.module.rules.push({
+            enforce: 'pre',
+            test: /.scss$/,
+            loader: 'sass-resources-loader',
+            options: {
+                resources: ['./theme/var.scss'],
+            }
+        });
+
+        return config;
+    }
+});
