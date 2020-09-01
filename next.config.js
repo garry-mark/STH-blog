7b0c91b9ad6be44abbd6df0ccc4512f6627114df
Garry Mark <94garrymark@gmail.com>
2020-09-01 09:42:03 +0800

    chore: 删除debug代码

--- next.config.js
+++ next.config.js
@@ -29,7 +29,6 @@ const nextConfig = {
                         }
                     }
                 });
-                console.log(config.module.rules)
                 return config;
             }
         }),
